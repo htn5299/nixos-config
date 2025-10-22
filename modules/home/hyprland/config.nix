4162ba4efbe5c51b16d0dcc9bbf1fc4b0e406a82
htn <htn5299@gmail.com>
2025-10-22 23:58:37 +0700

    update

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -34,7 +34,7 @@
       ];
       exec-once = [
         # "waybar"
-        "hyprctl dispatch workspace 1"
+        # "hyprctl dispatch workspace 1"
         "waypaper --restore"
         "wl-paste --type text --watch cliphist store"
         "wl-paste --type image --watch cliphist store"
